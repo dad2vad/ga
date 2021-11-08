a1f224d3ca05c44ff242de1821925c1247655070
dad <60132399+dad2vad@users.noreply.github.com>
2021-11-08 21:43:45 +0200

    Update hello-world.js

--- src/api/hello-world.js
+++ src/api/hello-world.js
@@ -1,6 +1,23 @@
+const getCircularReplacer = () => {
+  const seen = new WeakSet();
+  return (key, value) => {
+    if (typeof value === "object" && value !== null) {
+      if (seen.has(value)) {
+        return;
+      }
+      seen.add(value);
+    }
+    return value;
+  };
+};
+
+
+
+
 const sample = (req, res) => {
-console.log(req)
-  res.status(200).json({ message: "Hello, World!" })
+var v = JSON.stringify(req, getCircularReplacer(),4)
+console.log(v)
+  res.status(200).json(v)
 }
 
 export default sample
